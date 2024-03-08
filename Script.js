df4d57cb940caae876e4a12b72b62ccb73843df8
Jamesolukanni <157642564+Jamesolukanni@users.noreply.github.com>
2024-03-08 19:57:53 +0100

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,39 @@
+const inputText = document.getElementById("input-text");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if(inputText.value === ''){
+        alert("Kindly input the task you want to add today");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputText.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputText.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("close-task");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
